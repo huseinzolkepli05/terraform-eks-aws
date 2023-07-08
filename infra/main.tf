50e4cc2922bed0b9da519efb28c92657dffa3e6f
huseinzol05 <husein.zol05@gmail.com>
2023-07-08 21:48:50 +0800

    added role

--- infra/main.tf
+++ infra/main.tf
@@ -17,3 +17,33 @@ terraform {
 
   required_version = ">= 1.1.0"
 }
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
+
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "controlplane" {
+  name               = "eks-cluster"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "controlplane_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.controlplane.name
+}
